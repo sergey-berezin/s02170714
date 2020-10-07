dbfd562132ad5db038f84dc56e171c400e037a6c
Dai-ziqi <72304878+Dai-ziqi@users.noreply.github.com>
2020-10-08 01:13:54 +0800

    Update Threadworks.cs
    Use List<string> to save results, and override ToString() to output results.

--- ClassLibrary/Threadworks.cs
+++ ClassLibrary/Threadworks.cs
@@ -8,12 +8,14 @@ namespace ClassLibrary
     public class Threadworks
     {
         private static ManualResetEvent wait = new ManualResetEvent(false);
+        private static AutoResetEvent wait_for_write = new AutoResetEvent(false);
         private ConcurrentQueue<string> path_Imgs;
 
         private OnnxGeneral model;
         private string _path;
         public static int stop = 0;
-
+        private List<string> results = new List<string>();
+        
         public Threadworks(string path, OnnxGeneral onnxModel)
         {
             model = onnxModel;
@@ -61,20 +63,45 @@ namespace ClassLibrary
         {
             string Img_name;
             string th_name = Thread.CurrentThread.Name;
-
+            string _result;
+            
             while (path_Imgs.TryDequeue(out Img_name))
             {
                 if (wait.WaitOne(0))
                     break;
-                Console.WriteLine(th_name + ": " + model.PredictModel(OnnxGeneral.ImageModel(Img_name)) + ", file path: " + Img_name);
-            }
+                //Console.WriteLine(th_name + ": " + model.PredictModel(OnnxGeneral.ImageModel(Img_name)) + ", file path: " + Img_name);
 
+                result = th_name + ": " + model.PredictModel(OnnxGeneral.ImageModel(Img_name)) + ", file path: " + Img_name;                
+                if(th_name == "Thread_0")
+                {
+                    Thread.Sleep(500);
+                    results.Add(_result);
+                    wait_for_write.Set();
+                }
+                else
+                {
+                    wait_for_write.WaitOne();
+                    results.Add(_result);
+                    wait_for_write.Set();
+                }
+                
+            }
             if (stop == 0)
                 Console.WriteLine(th_name + " Stop normally !");
             else
                 Console.WriteLine(th_name + " Stop by Ctrl+C !");
         }
 
+        public override string ToString()
+        {
+            for(int i = 0; i < results.Count; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+            
+            return "\n";
+        }
+        
         protected static void myHandler(object sender, ConsoleCancelEventArgs args)
         {
             stop = 1;
